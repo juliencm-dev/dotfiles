a8b271d8643f471862d27ead934a24431aaa5a76
Julien Coulombe-Morency <hello@juliencm.dev>
2026-03-17 08:38:38 -0400

    update config to tokyo night

---
+++ tmuxifier/.tmux/plugins/tmuxifier/layouts/liftarchives.session.sh
@@ -0,0 +1,31 @@
+
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+OSX_HOME="/Users/juliencm"  # Remove spaces around the = sign
+if [ -n "$ZSH_VERSION" ]; then
+  # Running in ZSH
+  ACTUAL_HOME=$OSX_HOME
+  echo "ZSH detected, using actual home: $ACTUAL_HOME"
+else
+  # Running in other shells (bash, etc.)
+  ACTUAL_HOME="$HOME"
+  echo "Using standard HOME: $ACTUAL_HOME"
+fi
+
+session_root "$ACTUAL_HOME/project/perso/liftarchives"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "stuvy"; then
+  new_window "code"
+  run_cmd "cd $session_root && clear && nvim ."
+  new_window "claude"
+  run_cmd "cd $session_root && claude"
+  new_window "server"
+  run_cmd "cd $session_root && bun run dev:worker"
+  new_window "git"
+  run_cmd "cd $session_root && lazygit"
+  select_window 1
+fi
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
---
+++ tmuxifier/.tmux/plugins/tmuxifier/layouts/stuvy.session.sh
@@ -0,0 +1,32 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+OSX_HOME="/Users/juliencm"  # Remove spaces around the = sign
+if [ -n "$ZSH_VERSION" ]; then
+  # Running in ZSH
+  ACTUAL_HOME=$OSX_HOME
+  echo "ZSH detected, using actual home: $ACTUAL_HOME"
+else
+  # Running in other shells (bash, etc.)
+  ACTUAL_HOME="$HOME"
+  echo "Using standard HOME: $ACTUAL_HOME"
+fi
+
+session_root "$ACTUAL_HOME/project/cldstrt/cldstrt-crkdcln"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "stuvy"; then
+  new_window "server"
+  split_h 33
+  select_pane 1
+  run_cmd "cd $session_root/server && clear && nvim ."
+  new_window "client"
+  split_h 33
+  select_pane 1
+  run_cmd "cd $session_root/client && clear && nvim ."
+  new_window "git"
+  run_cmd "cd $session_root && lazygit"
+  select_window 1
+fi
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
